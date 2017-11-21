b11af0e2051e292365a7226b9fa6d259ceaa278c
RichardLee@apartment <richardlee0211er@gmail.com>
2017-11-20 19:46:08 -0800

    add Lab0

---
+++ Lab0/hw.c
@@ -0,0 +1,11 @@
+// hw.c
+#include <stdio.h>
+int main(){
+    printf("Hello World!\n");
+    int i;
+    for(i=0; i<100; i++){
+        //printf(%i, i);
+        printf("boring");
+    }
+    return 0;
+}
